50fc5035ae2f9532cb5ab4a84fc77dbcd2b50c0f
Tommy Waalkes <Tommy@grandcircus.co>
2018-10-29 15:00:09 -0400

    Made App into an object

---
+++ GuessingGameApp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus_3
+{
+    class GuessingGameApp
+    {
+        Random r = new Random();
+        private int num;
+        private int guessCount = 0;
+        public int GuessCount { get { return guessCount; } }
+        public GuessingGameApp()
+        {
+            num = r.Next(1, 101);
+            Console.WriteLine(num);
+        }
+
+        public GuessingGameApp(int num)
+        {
+            this.num = num;
+        }
+
+        public bool GuessAnswer(int input)
+        {
+            guessCount++;
+            int diff = 0;
+            if (input == num)
+            {
+                Console.WriteLine("Match! You win!");
+               
+                PrintGuesses();
+                return true;
+            }
+            else if (input > num)
+            {
+                diff = input - num;
+                if (diff > 10)
+                {
+                    Console.WriteLine("Way too high");
+                }
+                else
+                {
+                    Console.WriteLine("Too high!");
+                }
+            }
+            else
+            {
+                diff = num - input;
+                if (diff > 10)
+                {
+                    Console.WriteLine("Way too low!");
+                }
+                else
+                {
+                    Console.WriteLine("Too low");
+                }
+            }
+
+            return false;
+        }
+
+        public void PrintGuesses()
+        {
+            Console.WriteLine("You guessed " + guessCount + " times");
+            if (guessCount < 5)
+            {
+                Console.WriteLine("Wow you're a genius");
+            }
+            else if (guessCount < 10)
+            {
+                Console.WriteLine("Pretty solid!");
+            }
+            else if (guessCount < 20)
+            {
+                Console.WriteLine("Better luck next time, ya dangus");
+            }
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,64 +10,18 @@ namespace Bonus_3
     {
         static void Main(string[] args)
         {
-            Random r = new Random();
-
-            int mysNum = r.Next(1, 101);
-            Console.WriteLine(mysNum);
-            bool run = true;
-            int guesses = 0; 
-
-            while (run == true)
+           
+            bool foundNum = false;
+            
+            GuessingGameApp ga = new GuessingGameApp(10);
+            int i = 1;
+            while (foundNum == false)
             {
-                guesses++;
-                Console.WriteLine("Please input a guess");
-                int input = int.Parse(Console.ReadLine());
-                int diff = 0;
-                if (input == mysNum)
-                {
-                    Console.WriteLine("Match! You win!");
-                    break;
-                }
-                else if (input > mysNum)
-                {
-                    diff = input - mysNum;
-                    if (diff > 10)
-                    {
-                        Console.WriteLine("Way too high");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Too high!");
-                    }
-                }
-                else
-                {
-                    diff = mysNum - input;
-                    if (diff > 10)
-                    {
-                        Console.WriteLine("Way too low!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Too low");
-                    }
-                }
-
+                foundNum = ga.GuessAnswer(i);
+                i++;
             }
 
-            Console.WriteLine("You guessed " + guesses + " times");
-            if(guesses < 5)
-            {
-                Console.WriteLine("Wow you're a genius");
-            }
-            else if(guesses < 10)
-            {
-                Console.WriteLine("Pretty solid!");
-            }
-            else if(guesses < 20)
-            {
-                Console.WriteLine("Better luck next time, ya dangus");
-            }
+            
 
         }
     }
